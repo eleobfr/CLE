9babb31e7ed555d04e9f0d217164c2999ee337bd
eleobfr <62488125+eleobfr@users.noreply.github.com>
2026-03-31 23:51:03 +0200

    Improve responsive layout for main window

--- OutilWPF/MainWindow.xaml.cs
+++ OutilWPF/MainWindow.xaml.cs
@@ -23,12 +23,14 @@ namespace OutilWPF
             Title = options.Value.ApplicationTitle;
             Loaded += MainWindow_Loaded;
             DataContext = viewModel;
+            SizeChanged += MainWindow_SizeChanged;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var canOperate = databaseSelectionService.TryEnsureDatabasePath();
             viewModel.LoadDatas(canOperate);
+            ApplyResponsiveLayout();
         }
 
         private void Valider_Click(object sender, RoutedEventArgs e)
@@ -84,6 +86,37 @@ namespace OutilWPF
         private void Migrate_Click(object sender, RoutedEventArgs e)
         {
             viewModel.MigrerDonnées();
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ApplyResponsiveLayout();
+        }
+
+        private void ApplyResponsiveLayout()
+        {
+            if (!IsLoaded)
+                return;
+
+            var stackMainPanels = ActualWidth < 1450;
+            WorkColumnLeft.Width = stackMainPanels ? new GridLength(1, GridUnitType.Star) : new GridLength(330);
+            WorkColumnRight.Width = stackMainPanels ? new GridLength(0) : new GridLength(1, GridUnitType.Star);
+            WorkRowBottom.Height = stackMainPanels ? GridLength.Auto : new GridLength(0);
+            Grid.SetColumn(PatientsCard, 0);
+            Grid.SetRow(PatientsCard, 0);
+            Grid.SetColumn(DetailsCard, stackMainPanels ? 0 : 2);
+            Grid.SetRow(DetailsCard, stackMainPanels ? 1 : 0);
+            DetailsCard.Margin = stackMainPanels ? new Thickness(0, 18, 0, 0) : new Thickness(0);
+
+            var stackClinicalPanels = ActualWidth < 1680;
+            ClinicalColumnLeft.Width = stackClinicalPanels ? new GridLength(1, GridUnitType.Star) : new GridLength(320);
+            ClinicalColumnRight.Width = stackClinicalPanels ? new GridLength(0) : new GridLength(1, GridUnitType.Star);
+            ClinicalRowBottom.Height = stackClinicalPanels ? GridLength.Auto : new GridLength(0);
+            Grid.SetColumn(SessionsPanel, 0);
+            Grid.SetRow(SessionsPanel, 0);
+            Grid.SetColumn(TreatmentsPanel, stackClinicalPanels ? 0 : 2);
+            Grid.SetRow(TreatmentsPanel, stackClinicalPanels ? 1 : 0);
+            TreatmentsPanel.Margin = stackClinicalPanels ? new Thickness(0, 18, 0, 0) : new Thickness(0);
         }
     }
 }
